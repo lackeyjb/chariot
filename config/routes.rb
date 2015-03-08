2fda82fa6d4b3f8058d952d09c41591c07091be1
Martin Kikegawa <mkikegawa@gmail.com>
2015-03-08 11:02:45 -0400

    scaffold ride model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :rides, except: [:new, :edit]
   scope '/api' do
     resources :users, except: [:new, :edit]
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150306224719) do
+ActiveRecord::Schema.define(version: 20150308135309) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "rides", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "start_address"
+    t.string   "end_address"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
