05c215de712a020a03c5c74c22a09bd34300ef49
Bryan Lackey <bryan.lackey@gmail.com>
2015-03-10 15:06:32 -0400

    adds geolocation to client side

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -16,7 +16,8 @@ angular
     'ngRoute',
     'ngSanitize',
     'ngTouch',
-    'ng-token-auth'
+    'ng-token-auth',
+    'geolocation'
   ])
   .config(function ($routeProvider) {
     $routeProvider
